da06341947d02c1c4b9c649a81c0d89ba46df5ae
jaky-sheikh <jaky@eu4m.eu>
2025-02-11 23:47:29 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -28,13 +28,41 @@ async function run() {
 
         const equipmentCollection = client.db('equipmentDB').collection('equipments');
 
+        // add equipments
         app.post('/equipments', async (req, res) => {
             const newEquipment = req.body;
             const result = await equipmentCollection.insertOne(newEquipment);
             res.send(result);
         })
 
+        // equipment list
+        app.get("http://localhost:5000/equipments/user", async (req, res) => {
+            try {
+                const userEmail = req.query.email;
+                if (!userEmail) return res.status(400).json({ error: "User email required" });
 
+                const userEquipments = await equipmentCollection.find({ userEmail }).toArray();
+                res.status(200).json(userEquipments);
+            } catch (error) {
+                res.status(500).json({ error: "Failed to fetch equipments" });
+            }
+        });
+
+        //   delete equipment list
+        app.delete("http://localhost:5000/equipments/:id", async (req, res) => {
+            try {
+                const id = req.params.id;
+                const result = await equipmentCollection.deleteOne({ _id: new ObjectId(id) });
+
+                if (result.deletedCount === 1) {
+                    res.status(200).json({ message: "Equipment deleted successfully" });
+                } else {
+                    res.status(404).json({ error: "Equipment not found" });
+                }
+            } catch (error) {
+                res.status(500).json({ error: "Failed to delete equipment" });
+            }
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
