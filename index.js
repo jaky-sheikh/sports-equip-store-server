f4dfb0998d680766bc620d9d3270745daa606cbd
jaky-sheikh <jaky@eu4m.eu>
2025-02-11 16:08:26 +0600

    mongodb added in serverside

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlware
 app.use(cors());
 app.use(express.json());
+
+// EquiSports
+// Axn9S9DlorAhWej9
+
+
+
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.gi5sj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://EquiSports:Axn9S9DlorAhWej9@cluster0.gi5sj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Sports equipment server is running')
