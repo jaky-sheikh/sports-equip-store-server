a736acbd036c9ba8c1a4dd1a2dbdcb20e48c4a13
jaky-sheikh <jaky@eu4m.eu>
2025-03-05 15:08:50 +0600

    get operation for update

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
             res.json(result);
         })
 
+        // get operation for update
+        app.get('/equipments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await equipmentCollection.findOne(query);
+            res.send(result);
+        })
+
         // add equipments
         app.post('/equipments', async (req, res) => {
             const newEquipment = req.body;
